1e17e8df842d50e14892338264bd255d42476d08
Lucas Carvalho <lucasaugustomcc@gmail.com>
2018-05-25 08:06:36 -0700

    update component manager

--- scripts/wings-api/manage_component.py
+++ scripts/wings-api/manage_component.py
@@ -13,17 +13,18 @@ parser.add_argument("-n", "--new", help="Create operation", action="store_true")
 parser.add_argument("-c", "--component_id", help="Component id")
 parser.add_argument("-ct", "--component_type", help="Component type")
 parser.add_argument("-save", "--save_component", help="Save or update component info (filename with json data)")
-parser.add_argument("-init", "--initialize_code", help="Initialize component code")
+parser.add_argument("-init", "--initialize_code", help="Initialize component code", action="store_true")
 parser.add_argument("-lang", "--language_code", help="Programming Language", default="Generic")
 parser.add_argument("-code", "--save_code", help="Save component code")
 parser.add_argument("-path", "--script_path", help="Define script path location", default="run")
 parser.add_argument("-pt", "--parent_type", help="Component type's Parent (for creating new component type)")
 parser.add_argument("-pc", "--parent_component_id", help="Component's Parent (for creating new component)")
 parser.add_argument("-g", "--get", help="Get information (use either with component_id, component_type or none)", action="store_true")
+parser.add_argument("-l", "--location", help="Location to user folder")
 args = parser.parse_args()
 
 # Create manage user api
-component = wings.ManageComponent(args.server, args.userid, args.domain)
+component = wings.ManageComponent(args.server, args.userid, args.domain, args.location)
 
 # Login with password
 if component.login(args.password): 
@@ -31,13 +32,13 @@ if component.login(args.password):
 		if args.component_id:
 			component.add_component_for_type(args.component_id, args.parent_type, args.parent_component_id)
 		elif args.component_type:
-			component.new_component_type(args.component_type, args.parent_type)
+			component.add_component_type(args.component_type, args.parent_type)
 	elif args.save_component:
 		with open(args.save_component) as dfile:    
 			jsonobj = json.load(dfile)
-			component.save_component(jsonobj)
+			component.save_component(args.component_id, jsonobj)
 	elif args.initialize_code and args.component_id:
-			component.inicialize_component_code(args.component_id, args.language_code)
+			component.initialize_component_code(args.component_id, args.language_code)
 	elif args.save_code and args.component_id:
 			with open(args.save_code) as dfile:
 				component.save_component_code(args.component_id, dfile.read(), args.script_path)
--- scripts/wings-api/wings/component.py
+++ scripts/wings-api/wings/component.py
@@ -5,34 +5,45 @@ from .userop import UserOperation
 
 class ManageComponent(UserOperation):
 
-	def __init__(self, server, userid, domain):
+	def __init__(self, server, userid, domain, location=""):
 		super(ManageComponent, self).__init__(server, userid, domain)
-		self.cclib = self.get_export_url() + "components/library.owl#"
+		self.libns = self.get_export_url() + "components/library.owl#"
+		self.dcdom = self.get_export_url() + "data/ontology.owl#"
+		self.xsdns = "http://www.w3.org/2001/XMLSchema#"
+		self.topcls = "http://www.wings-workflows.org/ontology/component.owl#Component"
+		self.location = location + "/storage/users/" + userid + "/" + domain + "/code/library/"
 
 	def get_type_id(self, typeid):
-		if not re.match( r"(http:|https:)//", typeid ):
-			return self.cclib + typeid
+		if typeid == None:
+			return self.topcls
+		elif not re.match( r"(http:|https:)//", typeid ):
+			return self.libns + typeid
 		else:
 			return typeid
 
-	def get_component_id(self, cid):
-		if not re.match( r"(http:|https:)//", cid ):
-			return self.cclib + cid
+	def get_component_id(self, cid=None):
+		if cid == None:
+			return "" 
+		elif not re.match( r"(http:|https:)//", cid ):
+			return self.libns + cid
 		else:
 			return cid
 
-	def add_component_type(self, ctype, parent):
-		parent = self.get_type_id(parent)
-		ctype = self.get_type_id(ctype)
-		postdata = { 'parent_type': parent , 'cid': ctype }
-		self.session.post( self.get_request_url() + 'components/type/addComponent', postdata )
-
-	def add_component_for_type(self, cid, parent_type, parent_cid):
-		parent_cid = self.get_component_id(parent_cid)
+	def add_component_type(self, cid, parent_type=None, parent_cid=None):
 		parent_type = self.get_type_id(parent_type)
+		parent_cid = self.get_type_id(parent_cid)
+		cid = self.get_type_id(cid)
+		postdata = { 'parent_type': parent_type , 'parent_cid': parent_cid, 'cid': cid }
+		resp = self.session.post( self.get_request_url() + 'components/type/addComponent', postdata )
+		assert resp.content == "OK"
+
+	def add_component_for_type(self, cid, parent_cid, parent_type=None):
+		parent_cid = self.get_component_id(parent_cid)
+		parent_type = parent_cid + "Class"
 		cid = self.get_component_id(cid)
-		postdata = { 'parent_cid': parent_cid, 'parent_type': parent_type, 'cid': cid }
-		self.session.post( self.get_request_url() + 'components/addComponent', postdata )
+		postdata = { 'cid': cid, 'load_concrete': True, 'parent_cid': parent_cid, 'parent_type': parent_type }
+		resp = self.session.post( self.get_request_url() + 'components/addComponent', postdata )
+		assert resp.content == "OK"
 
 	def get_component_type_description(self, ctype):
 		ctype = self.get_type_id(ctype)
@@ -49,14 +60,29 @@ class ManageComponent(UserOperation):
 	def initialize_component_code(self, cid, language="Generic"):
 		cid = self.get_component_id(cid)
 		postdata = { 'cid': cid, 'language': language }
-		resp = self.session.post( self.get_request_url() + 'components/fb/initialize', postdata )
-		return resp.json()
+		self.session.post( self.get_request_url() + 'components/fb/initialize', postdata )
 
 	def save_component_code(self, cid, code, path="run"):
 		cid = self.get_component_id(cid)
 		postdata = { 'cid': cid, 'path': path, 'filedata': code }
 		resp = self.session.post( self.get_request_url() + 'components/fb/save', postdata )
 
-	def save_component(self, metadata):
-		postdata = { 'component_json': json.dumps(metadata) }
-		self.session.post( self.get_request_url() + 'components/saveComponentJSON', postdata )
+	def save_component(self, cid, metadata):
+		jsonobj = self._modify_component_json(cid, metadata)
+		jsonobj["type"] = 2
+		postdata = { 'load_concrete': True, 'component_json': json.dumps(jsonobj) }
+		resp = self.session.post( self.get_request_url() + 'components/saveComponentJSON', postdata )
+		assert resp.content == "OK"
+
+	def _modify_component_json(self, cid, jsonobj):
+		for input in jsonobj["inputs"]:
+			input["type"] = input["type"].replace("xsd:", self.xsdns)
+			input["type"] = input["type"].replace("dcdom:", self.dcdom)
+			input["id"] = self.get_component_id(cid) + "_" + input["role"]
+		for output in jsonobj["outputs"]:
+			output["type"] = output["type"].replace("xsd:", self.xsdns)
+			output["type"] = output["type"].replace("dcdom:", self.dcdom)
+			output["id"] = cid + "_" + output["role"]
+		jsonobj["id"] = self.get_component_id(cid)
+		jsonobj["location"] = self.location + cid
+		return jsonobj
